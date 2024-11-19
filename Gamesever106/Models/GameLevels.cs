3a0eee25ed98d1de57dd0b4d420ec11af3ebe666
huylqpd09954 <huylqpd09954@gmail.com>
2024-11-19 14:49:26 +0700

    first commit

---
+++ Gamesever106/Models/GameLevels.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gamesever106.Models
+{
+    public class GameLevels
+    {
+        [Key]
+        public int LevelId { get; set; }
+        public string title { get; set; }
+        public string? Description { get; set; }
+    }
+}
---
+++ Gamesever106/Models/Region.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gamesever106.Models
+{
+    public class Region
+    {
+        public int RegionId { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
